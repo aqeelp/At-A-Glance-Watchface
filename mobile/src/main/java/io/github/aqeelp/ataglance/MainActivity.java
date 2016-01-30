39ad7033480986ce07cece7a28d180721937a260
Aqeel <aqeelp@princeton.edu>
2016-01-29 21:18:56 -0500

    things broken now

--- mobile/src/main/java/io/github/aqeelp/ataglance/MainActivity.java
+++ mobile/src/main/java/io/github/aqeelp/ataglance/MainActivity.java
@@ -1,20 +1,9 @@
 package io.github.aqeelp.ataglance;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.wearable.DataMap;
-import com.google.android.gms.wearable.MessageApi;
-import com.google.android.gms.wearable.Node;
-import com.google.android.gms.wearable.NodeApi;
-import com.google.android.gms.wearable.Wearable;
 
 public class MainActivity extends AppCompatActivity {
 
